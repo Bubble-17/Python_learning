7d20e4ec916472f0a0b7ebb8abeac23d031ad27b
Bubble-17 <18286970655@163.com>
2026-06-22 23:17:22 +0800

    Add Python exercises for basic programming concepts
    Added Python exercises for printing, variable creation, reassignment, and arithmetic operations.

---
+++ day01.py
@@ -0,0 +1,30 @@
+#练习1：打印一句话
+print("今天是我自己跟着ai学Python的第1天")
+print("今天我终于连上网了，我居然一个人装好了家里的宽带，你真棒田娇")
+#练习2：创建变量
+name="田娇"
+age=22#其实我已经22岁了，我居然22岁了
+print(name)
+print(age)
+#练习3：变量可以重新赋值
+score=70
+print(score)
+score=95
+print(score)
+#练习4：变量之间可以运算
+a=5
+b=20
+c=a/b#哇塞，我发现和❓在一起的那个是除号➗
+print(c)
+#接下来是我的练习时间，自己操作啦！加油
+student_name="泡芙"#wok刚才没加双引号一直弹出错误undefined 泡芙，我以为是我代码没写完，它找不到
+english_scroe=129
+math_score=130
+amount=english_scroe+math_score
+print(amount)
+print(student_name+"的总分是" + str(amount))
+#TypeError: can only concatenate str (not 'int'),Python 不允许直接将字符串和整数拼接在一起。
+#通过使用 str() 函数将整数 age 转换为字符串，然后再与其他字符串拼接。
+
+
+
